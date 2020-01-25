6590f847bc236830120fb889f71f0b70af413642
SamuelMHarvey <59981663+SamuelMHarvey@users.noreply.github.com>
2020-01-24 22:48:23 -0600

    Add project files.

---
+++ RPGForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventure
+{
+    public partial class RPGForm : Form
+    {
+        public RPGForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RPGForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
